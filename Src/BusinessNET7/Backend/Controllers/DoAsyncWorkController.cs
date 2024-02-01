2c7ad7695025930bd74547051ab0e38dd62cc952
Vulcan <vulcan.lee@gmail.com>
2024-02-01 09:46:43 +0800

    DoAsyncWorkController

---
+++ Src/BusinessNET7/Backend/Controllers/DoAsyncWorkController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoAsyncWorkController : ControllerBase
+    {
+        [HttpGet("{waitPeriodOfTime}")]
+        public async Task<string> GetAsync([FromRoute] int waitPeriodOfTime)
+        {
+            try
+            {
+                await Task.Delay(waitPeriodOfTime, HttpContext.RequestAborted);
+                System.Console.WriteLine($" DoAsyncWork 將要完成");
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            return "Hello~~";
+        }
+    }
+}
